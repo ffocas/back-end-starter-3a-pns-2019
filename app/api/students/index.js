f667d7841eed83554bb381fe4c661baf14052e2e
Florian Focas <florianfocas@gmail.com>
2019-03-18 22:58:12 +0100

    presque tout fini

--- app/api/students/index.js
+++ app/api/students/index.js
@@ -3,14 +3,43 @@ const { Student, Ticket } = require('../../models');
 
 function attachTickets(student) {
   return Object.assign({ }, student, {
-    tickets: Ticket.get().filter(el => el.studentId === student.id)
+    tickets: Ticket.get().filter(el => el.studentId === student.id),
   });
 }
+
+function attachTicketsMultiple(students) {
+  return students.map(attachTickets);
+}
+
+function getByFirstName(firstname) {
+  return Student.get().filter(student => student.firstname === firstname);
+}
+
+function getByLastName(lastname) {
+  return Student.get().filter(student => student.lastname === lastname);
+}
+
+/*
+function getByBoth(firstname, lastname) {
+  return Student.get().filter((student) => {
+    student.firstname === firstname && student.lastname === lastname;
+  });
+}
+*/
 
 const router = new Router();
 router.get('/', (req, res) => res.status(200).json(Student.get().map(attachTickets)));
 router.get('/:studentId', (req, res) => res.status(200).json(attachTickets(Student.getById(req.params.studentId))));
-router.delete('/:studentId', (req, res) => res.status(200).json(attachTickets(Student.delete(req.params.studentId))));
+router.get('/firstname/:firstname', (req, res) => res.status(200).json(attachTicketsMultiple(getByFirstName(req.params.firstname))));
+router.get('/lastname/:lastname', (req, res) => {
+  res.status(200).json(attachTicketsMultiple(getByLastName(req.params.lastname)));
+});
+/*
+router.get('/firstname/:firstname/lastname/:lastname', (req, res) => {
+  res.status(200).json(attachTicketsMultiple(getByBoth(req.params.firstname, req.params.lastname)));
+});
+*/
+router.delete('/:studentId', (req, res) => res.status(200).json(Student.delete(req.params.studentId)));
 router.put('/:studentId', (req, res) => res.status(200).json(attachTickets(Student.update(req.params.studentId, req.body))));
 router.post('/', (req, res) => {
   try {
--- app/models/ticket.model.js
+++ app/models/ticket.model.js
@@ -3,6 +3,9 @@ const BaseModel = require('../utils/base-model.js');
 
 module.exports = new BaseModel('Ticket', {
   title: Joi.string().required(),
+  description: Joi.string().required(),
   date: Joi.date().required(),
+  archived: Joi.bool().required(),
+  major: Joi.string().required(),
   studentId: Joi.number().required(),
 });
